abea1ff5c40e8c7281ace3be111f814004655a4f
gustavao <elg.gespindola@gmail.com>
2020-03-01 11:25:25 -0600

    addQr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -19,6 +22,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   providers: [
     StatusBar,
+    BarcodeScanner,
+    Base64ToGallery,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/codi/codi.page.ts
+++ src/app/codi/codi.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
 
 @Component({
   selector: 'app-codi',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodiPage implements OnInit {
 
-  constructor() { }
+  codiForm: FormGroup;
+  //-----------------
+  qrData = "Banco del Bienestar";
+  scannedCode = null;
+  elementType: 'url' | 'canvas' | 'img' = 'canvas';
+  //-----------------
+
+  constructor(public formBuilder: FormBuilder, 
+              private barcodeScanner: BarcodeScanner, 
+            /**/  private base64ToGallery: Base64ToGallery) { 
+
+    this.codiForm = formBuilder.group({
+      payment: ["", Validators.compose([
+        Validators.required, 
+        Validators.minLength(9),
+        Validators.pattern('[0-9]')
+      ])],
+      concept: ["", Validators.compose([
+        Validators.required, 
+        Validators.minLength(3),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])],
+      reference: ["", Validators.compose([
+        Validators.required, 
+        Validators.minLength(3),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])]
+  });
+
+  }
 
   ngOnInit() {
+
   }
+
+  
+  createQr(){
+   /* this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,"Hola Mundo").then((data)=>{
+      alert(JSON.stringify(data));
+    },(err)=>{
+      alert(JSON.stringify(err));
+    }) */
+    this.qrData = this.codiForm.controls["payment"].value + 
+    this.codiForm.controls["concept"].value +
+    this.codiForm.controls["reference"].value
+  }
+/*
+  //scan test
+  scanCode() {
+    this.barcodeScanner.scan()
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((error) => {
+            alert(error);
+        });
+}
+*/
+scanCode(){
+  this.barcodeScanner.scan()
+        .then((result) => {
+            console.log(result);
+            this.scannedCode = result;
+        })
+        .catch((error) => {
+            alert(error);
+        });
+
+}
+
+downloadQr(){
+
+}
 
 }
