978cc191d0de4d79f273694a1e8e4154ca232077
gustavao <elg.gespindola@gmail.com>
2020-03-02 13:06:34 -0600

    estructura

---
+++ src/app/tranfers/tranfers.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tranfers',
+  templateUrl: './tranfers.page.html',
+  styleUrls: ['./tranfers.page.scss'],
+})
+export class TranfersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
