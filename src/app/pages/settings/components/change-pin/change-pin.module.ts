ab533e8693bd040f5d7832e9389106ff636db22c
sacha21 <sacha.n.alter@gmail.com>
2020-06-17 12:43:34 -0300

    PIN

--- src/app/pages/settings/components/change-pin/change-pin.module.ts
+++ src/app/pages/settings/components/change-pin/change-pin.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ChangePinPageRoutingModule } from './change-pin-routing.module';
 
 import { ChangePinPage } from './change-pin.page';
+import { InputErrorModule } from '@components/input-error/input-error.module';
+import { TranslateModule } from '@ngx-translate/core';
+import { SvgModule } from '@components/svg/svg.module';
+import { FooterModule } from '@components/footer/footer.module';
+import { HeaderModule } from '@components/header/header.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ChangePinPageRoutingModule
+    ChangePinPageRoutingModule,
+    ReactiveFormsModule,
+    InputErrorModule,
+    TranslateModule,
+    SvgModule,
+    FooterModule,
+    HeaderModule
   ],
   declarations: [ChangePinPage]
 })
